d09a15d0ac169a63f0be24cc7b5309a235b6c47b
Lucas Guedes <lucassilva@eng.ci.ufpb.br>
2021-01-22 17:25:52 -0300

    Fixando orientação do app para que fique apenas em portrait.

--- lib/mainTabView.dart
+++ lib/mainTabView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:victor_hugo_app_prototype/boxesFloor.dart';
 import 'package:victor_hugo_app_prototype/generalData.dart';
+import 'package:flutter/services.dart';
 
 //ignore: must_be_immutable
 class MainTabView extends StatelessWidget{
@@ -11,6 +12,11 @@ class MainTabView extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return DefaultTabController(
         length: 3,
         child: Scaffold(
